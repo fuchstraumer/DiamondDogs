c1d53518ee22410493efe487507cefcd9a4c9f80
kjones <kjones@tethers.com>
2017-07-05 11:39:14 -0700

    Facelift for CommandPool and LogicalDevice classes.

--- DiamondDogs/engine/renderer/command/CommandPool.h
+++ DiamondDogs/engine/renderer/command/CommandPool.h
@@ -10,43 +10,46 @@ namespace vulpes {
 	class CommandPool : public NonCopyable {
 	public:
 
-		CommandPool(const Device* parent, const VkCommandPoolCreateInfo& create_info, bool primary = true);
-
-		CommandPool(const Device* parent, bool primary = true);
-
-		void Create();
-		void Reset();
-
+		CommandPool(const Device* parent, const VkCommandPoolCreateInfo& create_info, bool primary);
+		CommandPool(const Device* parent, bool primary);
 		CommandPool(CommandPool&& other) noexcept;
 		CommandPool& operator=(CommandPool&& other) noexcept;
 
 		virtual ~CommandPool();
-
+		// Resets the CommandPool to the state it's in when constructed: invalid for use.
 		void Destroy();
 
-		void CreateCommandBuffers(const uint32_t& num_buffers, const VkCommandBufferAllocateInfo& alloc_info = vk_command_buffer_allocate_info_base);
+		// Creates command pool handle: does not allocate buffers.
+		void Create();
 
+		void AllocateCmdBuffers(const uint32_t& num_buffers, const VkCommandBufferAllocateInfo& alloc_info = vk_command_buffer_allocate_info_base);
+
+		void ResetCmdPool();
+		void ResetCmdBuffer(const size_t& idx);
+
+		// Freeing command buffers makes them invalid, unlike resetting.
 		void FreeCommandBuffers();
 
-		void ResetCommandBuffer(const size_t& idx);
-
 		const VkCommandPool& vkHandle() const noexcept;
-		const VkCommandBuffer& operator[](const size_t& idx) const;
 
+		VkCommandBuffer& operator[](const size_t& idx);
 		VkCommandBuffer& GetCmdBuffer(const size_t& idx);
 		const std::vector<VkCommandBuffer>& GetCmdBuffers() const;
 
 		VkCommandBuffer StartSingleCmdBuffer();
 		void EndSingleCmdBuffer(VkCommandBuffer& cmd_buffer, const VkQueue & queue);
+
 		const size_t size() const noexcept;
 
 	protected:
+
 		std::vector<VkCommandBuffer> cmdBuffers;
 		VkCommandPool handle;
 		VkCommandPoolCreateInfo createInfo;
 		const Device* parent;
 		const VkAllocationCallbacks* allocators = nullptr;
 		bool primary;
+
 	};
 
 
