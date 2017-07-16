61cad49aabd0e253db07c56a8af21456515a61aa
Keegan <jonesk74@uw.edu>
2017-07-15 20:07:21 -0700

    Removing all vestiges of rendering functionality from the AABB class: overly complex and prone to failure.

--- DiamondDogs/engine/util/AABB.h
+++ DiamondDogs/engine/util/AABB.h
@@ -21,33 +21,8 @@ namespace vulpes {
 
 			glm::vec3 Center() const;
 
-			void CreateMesh();
-
 			void UpdateMinMax(const float& y_min, const float& y_max);
 
-			void Render(VkCommandBuffer & cmd);
-
-			static void SetupRenderData(const Device* dvc, const VkRenderPass& renderpass, const Swapchain* swapchain, const glm::mat4& projection);
-			static void RenderAABBs(const glm::mat4& view, VkCommandBuffer& cmd, const VkViewport& viewport, const VkRect2D& scissor);
-			static void CleanupVkResources();
-			static VkPipelineLayout pipelineLayout;
-
-			static const Device* device;
-			static std::unique_ptr<PipelineCache> cache;
-			static std::unique_ptr<ShaderModule> vert, frag;
-			static std::unique_ptr<GraphicsPipeline> pipeline;
-			mesh::Mesh<> mesh;
-			
-			struct ubo_data {
-				glm::mat4 model;
-				glm::mat4 view;
-				glm::mat4 projection;
-			};
-
-			static ubo_data uboData;
-
-			static const VkAllocationCallbacks* allocators;
-			static std::unordered_multimap<glm::ivec3, AABB*> aabbPool;
 		};
 
 		
