529511ae3ea2d3377a368ce2bf244644b1dc962e
Keegan <jonesk74@uw.edu>
2017-11-12 12:03:48 -0800

    Restructure project: make folder structure follow convention, and begin a CMakeLists file.

---
+++ src/DiamondDogs.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "scenes\Terrain.h"
+#include "scenes\StarScene.h"
+#include "engine/subsystems/terrain/HeightNode.hpp"
+INITIALIZE_EASYLOGGINGPP
+
+
+
+int main() {
+
+	el::Configurations conf("logging.ini");
+	el::Loggers::reconfigureAllLoggers(conf);
+
+	auto wait_test_func = []() {
+		std::this_thread::sleep_for(std::chrono::milliseconds(250));
+		std::cout << "Waited for 250ms...";
+	};
+
+	terrain_scene::TerrainScene scene;
+	scene.RenderLoop();
+
+	return 0;
+}
