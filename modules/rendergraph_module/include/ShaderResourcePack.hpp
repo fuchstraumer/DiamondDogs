b3694422e3185609513a857e00491a5c57a2deda
Keegan <jonesk74@uw.edu>
2018-09-21 15:05:38 -0700

    Merging ShaderResourceCache into ShaderResourcePack

--- modules/rendergraph_module/include/ShaderResourcePack.hpp
+++ modules/rendergraph_module/include/ShaderResourcePack.hpp
@@ -15,6 +15,7 @@ namespace st {
 }
 
 class RenderGraph;
+struct VulkanResource;
 
 /*
     Creates descriptor sets, layouts, and a singular descriptor pool
@@ -28,8 +29,8 @@ public:
 
     // ShaderPacks aren't owned/loaded by this object: they are cached/stored elsewhere
     ShaderResourcePack(RenderGraph& _graph, const st::ShaderPack* pack);
-    ShaderResourcePack(ShaderResourcePack&& other) noexcept;
-    ShaderResourcePack& operator=(ShaderResourcePack&& other) noexcept;
+    ShaderResourcePack(ShaderResourcePack&& other) noexcept = default;
+    ShaderResourcePack& operator=(ShaderResourcePack&& other) noexcept = default;
     ~ShaderResourcePack();
 
     vpr::DescriptorSet* DescriptorSet(const char* rsrc_group_name) noexcept;
@@ -38,14 +39,25 @@ public:
     const vpr::DescriptorPool* DescriptorPool() const noexcept;
     std::vector<VkDescriptorSet> ShaderGroupSets(const char* shader_group_name) const noexcept;
 
+    const VulkanResource* At(const std::string& group_name, const std::string& name) const;
+    const VulkanResource* Find(const std::string& group_name, const std::string& name) const noexcept;
+    bool Has(const std::string& group_name, const std::string& name) const noexcept;
+
 private:
 
-    void createResources(const std::vector<const st::ShaderResource*>& resources);
     void createDescriptorPool();
-    void createSingleSet(const std::string& name);
     void createSets();
-
+    void createSingleSet(const std::string& name);
     void getGroupNames();
+
+    void createResources(const std::vector<const st::ShaderResource*>& resources);
+    void createResource(const st::ShaderResource * rsrc);
+    void createSampledImage(const st::ShaderResource* rsrc);
+    void createTexelBuffer(const st::ShaderResource * texel_buffer, bool storage);
+    void createUniformBuffer(const st::ShaderResource * uniform_buffer);
+    void createStorageBuffer(const st::ShaderResource * storage_buffer);
+    void createCombinedImageSampler(const st::ShaderResource* rsrc);
+    void createSampler(const st::ShaderResource* rsrc);
 
     RenderGraph& graph;
     std::unique_ptr<vpr::DescriptorPool> descriptorPool;
@@ -53,6 +65,7 @@ private:
     std::unordered_map<std::string, std::set<size_t>> shaderGroupSetIndices;
     std::vector<std::unique_ptr<vpr::DescriptorSet>> descriptorSets;
     std::vector<std::unique_ptr<vpr::DescriptorSetLayout>> setLayouts;
+    std::unordered_map<std::string, std::unordered_map<std::string, VulkanResource*>> resources;
     const st::ShaderPack* shaderPack;
 };
 
