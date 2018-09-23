0ecc55caa8874bd036a657e13f63513bdd49212b
Keegan <jonesk74@uw.edu>
2018-09-23 12:13:33 -0700

    Add tagged_bool, which acts like an enum that downcasts to a bool

---
+++ modules/common/tagged_bool.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef DIAMOND_DOGS_TAGGED_BOOL_HPP
+#define DIAMOND_DOGS_TAGGED_BOOL_HPP
+
+template<typename Tag>
+class tagged_bool {
+    bool value;
+    template<typename>
+    friend class tagged_bool;
+public:
+    constexpr explicit tagged_bool(bool v) : value{v} {}
+    constexpr explicit tagged_bool(int) = delete;
+    constexpr explicit tagged_bool(double) = delete;
+    constexpr explicit tagged_bool(void*) = delete;
+
+    template<typename OtherTag>
+    constexpr explicit tagged_bool(tagged_bool<OtherTag> b) : value{b.value} {}
+
+    constexpr explicit operator bool() const noexcept { return value; }
+    constexpr tagged_bool operator!() const noexcept { return tagged_bool{!value}; }
+
+    friend constexpr bool operator==(tagged_bool l, tagged_bool r) { return l.value == r.value; }
+    friend constexpr bool operator!=(tagged_bool l, tagged_bool r) { return l.value != r.value; }
+    
+};
+
+#endif //!DIAMOND_DOGS_TAGGED_BOOL_HPP
