abee4d925e41f958b4547768dd4daccfc215a973
Keegan <jonesk74@uw.edu>
2018-11-12 15:09:01 -0800

    Add DescriptorTest

---
+++ tests/integration_tests/DescriptorTest/DescriptorTest.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef DESCRIPTOR_TEST_HPP
+#define DESCRIPTOR_TEST_HPP
+#include "VulkanScene.hpp"
+
+class DescriptorTest : public VulkanScene {
+public:
+
+    static DescriptorTest& Get();
+
+};
+
+#endif // !DESCRIPTOR_TEST_HPP
