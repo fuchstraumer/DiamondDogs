1cc4da2f81621a12a61c0737963bcc68fe792f05
Keegan <jonesk74@uw.edu>
2019-01-02 13:40:03 -0800

    Work on implementation of new Descriptor

--- modules/rendergraph_module/include/Descriptor.hpp
+++ modules/rendergraph_module/include/Descriptor.hpp
@@ -4,13 +4,57 @@
 #include "ForwardDecl.hpp"
 #include <memory>
 #include <vulkan/vulkan.h>
+#include "DescriptorTemplate.hpp"
+
 struct VulkanResource;
+
+namespace st {
+    struct descriptor_type_counts_t;
+}
+
+struct DescriptorHandle {
+    DescriptorHandle(const DescriptorHandle&) = delete;
+    DescriptorHandle& operator=(const DescriptorHandle&) = delete;
+    ~DescriptorHandle();
+    void BindResourceToIdx(size_t idx, VkDescriptorType type, VulkanResource * rsrc);
+    void Update();
+    VkDescriptorSet Handle() const noexcept;
+
+private:
+    DescriptorHandle(class Descriptor& parent, VkDescriptorSet&& _handle);
+    friend class Descriptor;
+    Descriptor& parent;
+    VkDescriptorUpdateTemplate templHandle;
+    VkDescriptorSet handle;
+    UpdateTemplateData data;
+};
 
 class Descriptor {
 public:
 
-    void BindResourceToIdx(size_t idx, VulkanResource * rsrc);
+    Descriptor(const vpr::Device * _device, const st::descriptor_type_counts_t& rsrc_counts, size_t max_sets, const DescriptorTemplate* templ);
+    ~Descriptor();
+
+    // frees all sets
+    void Reset();
+    // frees sets moved to "usedSets"
+    void Trim();
+
+    DescriptorHandle GetHandle();
+    
 private:
+    friend class DescriptorHandle;
+
+    void allocateSets();
+    void createPool(const st::descriptor_type_counts_t& rsrc_counts);
+
+    size_t maxSets{ 0u };
+    const vpr::Device* device;
+    const DescriptorTemplate* templ;
+    std::unique_ptr<vpr::DescriptorPool> descriptorPool;
+    std::vector<VkDescriptorSet> availSets;
+    std::vector<VkDescriptorSet> usedSets;
+
 };
 
 #endif // !DIAMOND_DOGS_DESCRIPTOR_SET_HPP
--- modules/rendergraph_module/src/Descriptor.cpp
+++ modules/rendergraph_module/src/Descriptor.cpp
@@ -1,13 +1,88 @@
 #include "Descriptor.hpp"
+#include "LogicalDevice.hpp"
+#include "vpr/DescriptorPool.hpp"
+#include "common/UtilityStructs.hpp"
+#include "vkAssert.hpp"
 
-void Descriptor::BindResourceToIdx(size_t idx, VulkanResource* rsrc) {
-    // no resource bound at that index yet
-    if (createdBindings.count(idx) == 0) {
-        // make sure we do have the ability to bind here, though (entry is zero if not added to layout binding)
-        addDescriptorBinding(idx, rsrc);
-    }
-    else {
-        updateDescriptorBinding(idx, rsrc);
-    }
+DescriptorHandle::DescriptorHandle(Descriptor& _parent, VkDescriptorSet&& _handle) : parent(_parent), templHandle(parent.templ->UpdateTemplate()), handle(std::move(_handle)),
+    data(parent.templ->UpdateData()) {} // starts out with initial data of parent
 
+DescriptorHandle::~DescriptorHandle() {
+    parent.usedSets.emplace_back(handle);
+}
+
+void DescriptorHandle::BindResourceToIdx(size_t idx, VkDescriptorType type, VulkanResource * rsrc) {
+    data.BindResourceToIdx(idx, type, rsrc);
+}
+
+void DescriptorHandle::Update() {
+    vkUpdateDescriptorSetWithTemplate(parent.device->vkHandle(), handle, templHandle, data.Data());
+}
+
+VkDescriptorSet DescriptorHandle::Handle() const noexcept {
+    return handle;
+}
+
+Descriptor::Descriptor(const vpr::Device * _device, const st::descriptor_type_counts_t & rsrc_counts, size_t max_sets, const DescriptorTemplate* _templ) : device(_device), maxSets{ max_sets }, templ{ _templ } {
+    createPool(rsrc_counts);
+}
+
+Descriptor::~Descriptor() {
+    Reset();
+}
+
+void Descriptor::Reset() {
+    Trim();
+    if (!availSets.empty()) {
+        vkFreeDescriptorSets(device->vkHandle(), descriptorPool->vkHandle(), static_cast<uint32_t>(availSets.size()), availSets.data());
+        availSets.clear();
+    }
+}
+
+void Descriptor::Trim() {
+    if (usedSets.empty()) {
+        return;
+    }
+    vkFreeDescriptorSets(device->vkHandle(), descriptorPool->vkHandle(), static_cast<uint32_t>(usedSets.size()), usedSets.data());
+    usedSets.clear();
+}
+
+DescriptorHandle Descriptor::GetHandle() {
+    if (availSets.empty()) {
+        allocateSets();
+    }
+    VkDescriptorSet result_handle = availSets.back();
+    availSets.pop_back();
+    return DescriptorHandle(*this, std::move(result_handle));
+}
+
+void Descriptor::allocateSets() {
+    const VkDescriptorSetLayout layout = templ->SetLayout();
+    std::vector<VkDescriptorSetLayout> set_layouts(maxSets, templ->SetLayout());
+    const VkDescriptorSetAllocateInfo alloc_info{
+        VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO,
+        nullptr,
+        descriptorPool->vkHandle(),
+        maxSets,
+        set_layouts.data()
+    };
+    availSets.resize(maxSets, VK_NULL_HANDLE);
+    VkResult result = vkAllocateDescriptorSets(device->vkHandle(), &alloc_info, availSets.data());
+    VkAssert(result);
+    usedSets.reserve(maxSets);
+}
+
+void Descriptor::createPool(const st::descriptor_type_counts_t & rsrc_counts) {
+    descriptorPool = std::make_unique<vpr::DescriptorPool>(device->vkHandle(), maxSets);
+    descriptorPool->AddResourceType(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, rsrc_counts.UniformBuffers * maxSets);
+    descriptorPool->AddResourceType(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, rsrc_counts.UniformBuffersDynamic * maxSets);
+    descriptorPool->AddResourceType(VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, rsrc_counts.StorageBuffers * maxSets);
+    descriptorPool->AddResourceType(VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC, rsrc_counts.StorageBuffersDynamic * maxSets);
+    descriptorPool->AddResourceType(VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER, rsrc_counts.StorageTexelBuffers * maxSets);
+    descriptorPool->AddResourceType(VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER, rsrc_counts.UniformTexelBuffers * maxSets);
+    descriptorPool->AddResourceType(VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, rsrc_counts.StorageImages * maxSets);
+    descriptorPool->AddResourceType(VK_DESCRIPTOR_TYPE_SAMPLER, rsrc_counts.Samplers * maxSets);
+    descriptorPool->AddResourceType(VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, rsrc_counts.SampledImages * maxSets);
+    descriptorPool->AddResourceType(VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, rsrc_counts.CombinedImageSamplers * maxSets);
+    descriptorPool->Create();
 }
