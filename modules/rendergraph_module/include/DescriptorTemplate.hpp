dc1fbdfa4b6ec389c07ca946b2fddcbdebe0b2c1
Keegan <jonesk74@uw.edu>
2019-01-02 12:42:51 -0800

    Reduce responsibility and scope of UpdateTemplate

--- modules/rendergraph_module/include/DescriptorTemplate.hpp
+++ modules/rendergraph_module/include/DescriptorTemplate.hpp
@@ -1,13 +1,14 @@
 #pragma once
 #ifndef DIAMOND_DOGS_DESCRIPTOR_TEMPLATE_HPP
 #define DIAMOND_DOGS_DESCRIPTOR_TEMPLATE_HPP
-#include <string>
-#include <vulkan/vulkan.h>
 #include "ForwardDecl.hpp"
 #include <memory>
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+#include <string>
+#include <vulkan/vulkan.h>
+#include "UpdateTemplateData.hpp"
 
 struct VulkanResource;
 
@@ -22,29 +23,10 @@ public:
 
     void AddLayoutBinding(size_t idx, VkDescriptorType type);
     void AddLayoutBinding(VkDescriptorSetLayoutBinding binding);
-    void BindResourceToIdx(size_t idx, VulkanResource* rsrc);
+    void BindResourceToIdx(size_t idx, VkDescriptorType type, VulkanResource* rsrc);
     
 private:
 
-    union rawDataEntry {
-        rawDataEntry() = default;
-        rawDataEntry(VkDescriptorBufferInfo&& info) : BufferInfo(std::forward<VkDescriptorBufferInfo>(info)) {}
-        rawDataEntry(VkDescriptorImageInfo&& info) : ImageInfo(std::forward<VkDescriptorImageInfo>(info)) {}
-        rawDataEntry(VkBufferView view) : BufferView{ view } {}
-        VkDescriptorBufferInfo BufferInfo;
-        VkDescriptorImageInfo ImageInfo;
-        VkBufferView BufferView;
-    };
-
-    void updateDescriptorBinding(const size_t idx, VulkanResource* rsrc);
-    void updateBufferDescriptor(const size_t idx, VulkanResource * rsrc);
-    void updateImageDescriptor(const size_t idx, VulkanResource * rsrc);
-    void addDescriptorBinding(const size_t idx, VulkanResource* rsrc);
-    void addBufferDescriptor(const size_t idx, VulkanResource* rsrc);
-    void addSamplerDescriptor(const size_t idx, VulkanResource* rsrc);
-    void addImageDescriptor(const size_t idx, VulkanResource* rsrc);
-    void addCombinedImageSamplerDescriptor(const size_t idx, VulkanResource * img, VulkanResource * sampler);
-    void addRawEntry(const size_t idx, rawDataEntry&& entry);
     void addUpdateEntry(const size_t idx, VkDescriptorUpdateTemplateEntry&& entry);
     void createUpdateTemplate() const;
 
@@ -54,11 +36,9 @@ private:
     std::unique_ptr<vpr::DescriptorSetLayout> descriptorSetLayout{ nullptr };
     mutable VkDescriptorUpdateTemplate updateTemplate{ VK_NULL_HANDLE };
     mutable VkDescriptorUpdateTemplateCreateInfo templateInfo{ VK_STRUCTURE_TYPE_DESCRIPTOR_UPDATE_TEMPLATE_CREATE_INFO, nullptr };
-    std::vector<rawDataEntry> rawEntries;
+
+    UpdateTemplateData updateData;
     std::vector<VkDescriptorUpdateTemplateEntry> updateEntries;
-    std::unordered_map<VulkanResource*, size_t> resourceBindings;
-    std::unordered_map<size_t, VkDescriptorType> descriptorTypeMap;
-    std::unordered_set<size_t> createdBindings;
 
 };
 
--- modules/rendergraph_module/src/DescriptorTemplate.cpp
+++ modules/rendergraph_module/src/DescriptorTemplate.cpp
@@ -8,18 +8,6 @@
 #include "vkAssert.hpp"
 #include <cassert>
 
-static VkImageLayout imageLayoutFromUsage(const VkImageUsageFlags usage_flags) {
-    if (usage_flags & VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT) {
-        return VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
-    }
-    else if (usage_flags & VK_IMAGE_USAGE_SAMPLED_BIT) {
-        return VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-    }
-    else {
-        return VK_IMAGE_LAYOUT_GENERAL;
-    }
-}
-
 DescriptorTemplate::DescriptorTemplate(std::string _name) : name(std::move(_name)) {
     auto& ctxt = RenderingContext::Get();
     descriptorSetLayout = std::make_unique<vpr::DescriptorSetLayout>(ctxt.Device()->vkHandle());
@@ -33,32 +21,33 @@ DescriptorTemplate::~DescriptorTemplate() {
 
 void DescriptorTemplate::AddLayoutBinding(size_t idx, VkDescriptorType type) {
     assert(!created);
-    descriptorSetLayout->AddDescriptorBinding(type, VK_SHADER_STAGE_ALL, uint32_t(idx));
-    descriptorTypeMap.emplace(std::move(idx), std::move(type));
+    descriptorSetLayout->AddDescriptorBinding(type, VK_SHADER_STAGE_ALL, uint32_t(idx)); 
+    addUpdateEntry(uint32_t(idx), VkDescriptorUpdateTemplateEntry{
+        uint32_t(idx),
+        0,
+        1,
+        type,
+        sizeof(UpdateTemplateDataEntry) * idx,
+        0
+    });
 }
 
 void DescriptorTemplate::AddLayoutBinding(VkDescriptorSetLayoutBinding binding) {
     // can't add more bindings after init
     assert(!created);
     descriptorSetLayout->AddDescriptorBinding(binding);
-    descriptorTypeMap.emplace(binding.binding, binding.descriptorType);
+    addUpdateEntry(binding.binding, VkDescriptorUpdateTemplateEntry{
+        binding.binding,
+        0,
+        1,
+        binding.descriptorType,
+        sizeof(UpdateTemplateDataEntry) * binding.binding,
+        0
+    });
 }
 
-void DescriptorTemplate::BindResourceToIdx(size_t idx, VulkanResource* rsrc) {
-
-    if (!dirty) {
-        dirty = true;
-    }
-
-    // no resource bound at that index yet
-    if (createdBindings.count(idx) == 0) {
-        // make sure we do have the ability to bind here, though (entry is zero if not added to layout binding)
-        addDescriptorBinding(idx, rsrc);
-    }
-    else {
-        updateDescriptorBinding(idx, rsrc);
-    }
-
+void DescriptorTemplate::BindResourceToIdx(size_t idx, VkDescriptorType type, VulkanResource* rsrc) {
+    updateData.BindResourceToIdx(idx, type, rsrc);
 }
 
 VkDescriptorUpdateTemplate DescriptorTemplate::UpdateTemplate() const noexcept {
@@ -73,161 +62,8 @@ VkDescriptorSetLayout DescriptorTemplate::SetLayout() const {
 }
 
 void DescriptorTemplate::UpdateSet(VkDescriptorSet set) {
-    vkUpdateDescriptorSetWithTemplate(device->vkHandle(), set, updateTemplate, rawEntries.data());
-}
-
-void DescriptorTemplate::updateDescriptorBinding(const size_t idx, VulkanResource * rsrc) {
-
-    switch (rsrc->Type) {
-    case resource_type::BUFFER:
-        updateBufferDescriptor(idx, rsrc);
-        break;
-    case resource_type::IMAGE:
-        updateImageDescriptor(idx, rsrc);
-        break;
-    case resource_type::SAMPLER:
-        break;
-    case resource_type::COMBINED_IMAGE_SAMPLER:
-        updateImageDescriptor(idx, rsrc);
-        break;
-    default:
-        throw std::domain_error("VulkanResource pointer had invalid type value!");
-    }
-
-}
-
-void DescriptorTemplate::updateBufferDescriptor(const size_t idx, VulkanResource* rsrc) {
-
-    auto& raw_entry = rawEntries[idx];
-    if (rsrc->ViewHandle != VK_NULL_HANDLE) {
-        raw_entry = rawDataEntry((VkBufferView)rsrc->ViewHandle);
-    }
-    else {
-        raw_entry.BufferInfo.buffer = (VkBuffer)rsrc->Handle;
-        raw_entry.BufferInfo.range = reinterpret_cast<const VkBufferCreateInfo*>(rsrc->Info)->size;
-    }
-
-}
-
-void DescriptorTemplate::updateImageDescriptor(const size_t idx, VulkanResource* rsrc) {
-
-    auto& raw_entry = rawEntries[idx];
-    raw_entry.ImageInfo.imageView = (VkImageView)rsrc->ViewHandle;
-
-}
-
-void DescriptorTemplate::addDescriptorBinding(const size_t idx, VulkanResource* rsrc) {
-
-    switch (rsrc->Type) {
-    case resource_type::BUFFER:
-        addBufferDescriptor(idx, rsrc);
-        break;
-    case resource_type::SAMPLER:
-        addSamplerDescriptor(idx, rsrc);
-        break;
-    case resource_type::IMAGE:
-        addImageDescriptor(idx, rsrc);
-        break;
-    case resource_type::COMBINED_IMAGE_SAMPLER:
-        addCombinedImageSamplerDescriptor(idx, rsrc, rsrc->Sampler);
-    default:
-        throw std::domain_error("Invalid resource type when trying to add descriptor binding to DescriptorTemplate.");
-    };
-
-    createdBindings.emplace(idx);
-}
-
-void DescriptorTemplate::addBufferDescriptor(const size_t idx, VulkanResource* rsrc) {
-    const VkDescriptorType& type = descriptorTypeMap.at(idx);
-
-    if (type == VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER || type == VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER) {
-        addRawEntry(idx, rawDataEntry((VkBufferView)rsrc->ViewHandle));
-    }
-    else {
-        const uint32_t range = reinterpret_cast<const VkBufferCreateInfo*>(rsrc->Info)->size;
-        /*
-        constexpr uint32_t MAX_UNIFORM_BUFFER_RANGE_MIN = 16384u;
-        constexpr uint32_t MAX_STORAGE_BUFFER_RANGE_MIN = 134217728u;
-        uint32_t range = (type == VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER) ? MAX_UNIFORM_BUFFER_RANGE_MIN : MAX_STORAGE_BUFFER_RANGE_MIN;
-        */
-        addRawEntry(idx, rawDataEntry{ VkDescriptorBufferInfo {
-             (VkBuffer)rsrc->Handle,
-             0,
-             range
-        } });
-    }
-
-    addUpdateEntry(idx, VkDescriptorUpdateTemplateEntry{
-        uint32_t(idx),
-        0,
-        1,
-        type,
-        sizeof(rawDataEntry) * idx,
-        0 // size not required for single-descriptor entries
-        });
-
-}
-
-void DescriptorTemplate::addSamplerDescriptor(const size_t idx, VulkanResource* rsrc) {
-    addRawEntry(idx, rawDataEntry{});
-    addUpdateEntry(idx, VkDescriptorUpdateTemplateEntry{
-        uint32_t(idx),
-        0,
-        1,
-        VK_DESCRIPTOR_TYPE_SAMPLER,
-        sizeof(rawDataEntry) * idx,
-        0
-        });
-}
-
-void DescriptorTemplate::addImageDescriptor(const size_t idx, VulkanResource* rsrc) {
-    const VkImageCreateInfo* img_create_info = reinterpret_cast<VkImageCreateInfo*>(rsrc->Info);
-
-    addRawEntry(idx, rawDataEntry{
-        VkDescriptorImageInfo{
-            VK_NULL_HANDLE,
-            (VkImageView)rsrc->ViewHandle,
-            imageLayoutFromUsage(img_create_info->usage)
-        }
-        });
-
-    addUpdateEntry(idx, VkDescriptorUpdateTemplateEntry{
-        uint32_t(idx),
-        0,
-        1,
-        descriptorTypeMap.at(idx),
-        sizeof(rawDataEntry) * idx,
-        0
-        });
-
-}
-
-void DescriptorTemplate::addCombinedImageSamplerDescriptor(const size_t idx, VulkanResource* img, VulkanResource* sampler) {
-    const VkImageCreateInfo* img_create_info = reinterpret_cast<VkImageCreateInfo*>(img->Info);
-
-    addRawEntry(idx, rawDataEntry{ VkDescriptorImageInfo{
-        (VkSampler)sampler->Handle,
-        (VkImageView)img->ViewHandle,
-        imageLayoutFromUsage(img_create_info->usage)
-    } });
-
-    addUpdateEntry(idx, VkDescriptorUpdateTemplateEntry{
-        uint32_t(idx),
-        0,
-        1,
-        descriptorTypeMap.at(idx),
-        sizeof(rawDataEntry) * idx,
-        0
-        });
-
-    createdBindings.emplace(idx);
-}
-
-void DescriptorTemplate::addRawEntry(const size_t idx, rawDataEntry&& entry) {
-    if (rawEntries.empty() || idx >= rawEntries.size()) {
-        rawEntries.resize(idx + 1);
-    }
-    rawEntries[idx] = std::move(entry);
+    assert(updateData.Size() == updateEntries.size());
+    vkUpdateDescriptorSetWithTemplate(device->vkHandle(), set, updateTemplate, updateData.Data());
 }
 
 void DescriptorTemplate::addUpdateEntry(const size_t idx, VkDescriptorUpdateTemplateEntry&& entry) {
@@ -239,7 +75,6 @@ void DescriptorTemplate::addUpdateEntry(const size_t idx, VkDescriptorUpdateTemp
 
 void DescriptorTemplate::createUpdateTemplate() const {
     templateInfo.descriptorUpdateEntryCount = static_cast<uint32_t>(updateEntries.size());
-    assert(updateEntries.size() == rawEntries.size());
     templateInfo.pDescriptorUpdateEntries = updateEntries.data();
     templateInfo.templateType = VK_DESCRIPTOR_UPDATE_TEMPLATE_TYPE_DESCRIPTOR_SET;
     templateInfo.descriptorSetLayout = descriptorSetLayout->vkHandle();
