5219c2f61c470eb3f84c8544abd594771b0aa15f
Keegan <jonesk74@uw.edu>
2019-01-02 12:43:36 -0800

    Add UpdateTemplateData, for storing handle data for update templates
    Separated to another class as both the descriptor set object and the template object from which it is created will need this object

---
+++ modules/rendergraph_module/include/UpdateTemplateData.hpp
@@ -0,0 +1,46 @@
+#pragma once
+#ifndef DIAMOND_DOGS_UPDATE_TEMPLATE_DATA_HPP
+#define DIAMOND_DOGS_UPDATE_TEMPLATE_DATA_HPP
+#include <vulkan/vulkan.h>
+#include <utility>
+#include <vector>
+
+struct VulkanResource;
+
+union UpdateTemplateDataEntry {
+    UpdateTemplateDataEntry() = default;
+    UpdateTemplateDataEntry(const UpdateTemplateDataEntry&) noexcept = default;
+    UpdateTemplateDataEntry(UpdateTemplateDataEntry&&) noexcept = default;
+    UpdateTemplateDataEntry& operator=(UpdateTemplateDataEntry&&) noexcept = default;
+    explicit UpdateTemplateDataEntry(VkDescriptorBufferInfo&& info) noexcept : BufferInfo(std::move(info)) {}
+    explicit UpdateTemplateDataEntry(VkDescriptorImageInfo&& info) noexcept: ImageInfo(std::move(info)) {}
+    explicit UpdateTemplateDataEntry(VkBufferView view) noexcept : BufferView{ view } {}
+    VkDescriptorBufferInfo BufferInfo;
+    VkDescriptorImageInfo ImageInfo;
+    VkBufferView BufferView;
+};
+
+class UpdateTemplateData {
+public:
+
+    UpdateTemplateData() = default;
+    UpdateTemplateData(const UpdateTemplateData& other) noexcept;
+    UpdateTemplateData(UpdateTemplateData&& other) noexcept;
+    UpdateTemplateData& operator=(const UpdateTemplateData& other) noexcept;
+    UpdateTemplateData& operator=(UpdateTemplateData&& other) noexcept;
+
+    void BindResourceToIdx(size_t idx, VkDescriptorType type, VulkanResource* rsrc);
+    const void* Data() const noexcept;
+    size_t Size() const noexcept;
+
+private:    
+    
+    void bindSamplerDescriptor(const size_t idx, VulkanResource* rsrc);
+    void bindBufferDescriptor(const size_t idx, VkDescriptorType type, VulkanResource* rsrc);
+    void bindImageDescriptor(const size_t idx, VkDescriptorType type, VulkanResource * rsrc);
+    void bindSamplerDescriptor(const size_t idx, VulkanResource * rsrc);
+    std::vector<UpdateTemplateDataEntry> rawEntries;
+
+};
+
+#endif // !DIAMOND_DOGS_UPDATE_TEMPLATE_DATA_HPP
