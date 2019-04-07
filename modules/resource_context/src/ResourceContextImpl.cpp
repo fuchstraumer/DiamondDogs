1d6a77141b0b62d828a2db320fa83e1a972c7b60
Keegan <jonesk74@uw.edu>
2019-04-07 16:06:27 -0700

    Move to using simple Vulkan Sync in ResourceContext as well

--- modules/resource_context/src/ResourceContextImpl.cpp
+++ modules/resource_context/src/ResourceContextImpl.cpp
@@ -3,6 +3,58 @@
 #define VMA_IMPLEMENTATION
 #include <vk_mem_alloc.h>
 #include <fstream>
+#define THSVS_SIMPLER_VULKAN_SYNCHRONIZATION_IMPLEMENTATION
+#include <thsvs_simpler_vulkan_synchronization.h>
+
+static std::vector<ThsvsAccessType> thsvsAccessTypesFromBufferUsage(VkBufferUsageFlagBits _flags)
+{
+    std::vector<ThsvsAccessType> results;
+    if (_flags & VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT)
+    {
+        results.emplace_back(THSVS_ACCESS_ANY_SHADER_READ_UNIFORM_BUFFER);
+    }
+    if (_flags & VK_BUFFER_USAGE_INDEX_BUFFER_BIT)
+    {
+        results.emplace_back(THSVS_ACCESS_INDEX_BUFFER);
+    }
+    if (_flags & VK_BUFFER_USAGE_VERTEX_BUFFER_BIT)
+    {
+        results.emplace_back(THSVS_ACCESS_VERTEX_BUFFER);
+    }
+    if (_flags & VK_BUFFER_USAGE_INDEX_BUFFER_BIT)
+    {
+        results.emplace_back(THSVS_ACCESS_INDIRECT_BUFFER);
+    }
+    if (_flags & VK_BUFFER_USAGE_CONDITIONAL_RENDERING_BIT_EXT)
+    {
+        results.emplace_back(THSVS_ACCESS_CONDITIONAL_RENDERING_READ_EXT);
+    }
+    if (_flags & VK_BUFFER_USAGE_UNIFORM_TEXEL_BUFFER_BIT)
+    {
+        results.emplace_back(THSVS_ACCESS_ANY_SHADER_READ_SAMPLED_IMAGE_OR_UNIFORM_TEXEL_BUFFER);
+    }
+    if ((_flags & VK_BUFFER_USAGE_STORAGE_BUFFER_BIT) || (_flags & VK_BUFFER_USAGE_STORAGE_TEXEL_BUFFER_BIT))
+    {
+        results.emplace_back(THSVS_ACCESS_ANY_SHADER_WRITE);
+    }
+    if (_flags & VK_BUFFER_USAGE_TRANSFER_DST_BIT)
+    {
+        results.emplace_back(THSVS_ACCESS_TRANSFER_WRITE);
+    }
+    if (_flags & VK_BUFFER_USAGE_TRANSFER_SRC_BIT)
+    {
+        results.emplace_back(THSVS_ACCESS_TRANSFER_READ);
+    }
+
+    if (results.empty())
+    {
+        // Didn't match any flags. Go super general.
+        results.emplace_back(THSVS_ACCESS_ANY_SHADER_WRITE);
+        results.emplace_back(THSVS_ACCESS_ANY_SHADER_READ_OTHER);
+    }
+
+    return results;
+}
 
 static VkAccessFlags accessFlagsFromBufferUsage(VkBufferUsageFlags usage_flags)
 {
@@ -38,6 +90,55 @@ static VkAccessFlags accessFlagsFromBufferUsage(VkBufferUsageFlags usage_flags)
     {
         return VK_ACCESS_MEMORY_READ_BIT;
     }
+}
+
+static std::vector<ThsvsAccessType> thsvsAccessTypesFromImageUsage(VkImageUsageFlags _flags)
+{
+    std::vector<ThsvsAccessType> results;
+
+    if (_flags & VK_IMAGE_USAGE_SAMPLED_BIT)
+    {
+        results.emplace_back(THSVS_ACCESS_ANY_SHADER_READ_SAMPLED_IMAGE_OR_UNIFORM_TEXEL_BUFFER);
+    }
+    if (_flags & VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT)
+    {
+        results.emplace_back(THSVS_ACCESS_COLOR_ATTACHMENT_READ_WRITE);
+    }
+    if (_flags & VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT)
+    {
+        results.emplace_back(THSVS_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ);
+        results.emplace_back(THSVS_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE);
+    }
+    if (_flags & VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT)
+    {
+        results.emplace_back(THSVS_ACCESS_COLOR_ATTACHMENT_READ);
+    }
+    if (_flags & VK_IMAGE_USAGE_SHADING_RATE_IMAGE_BIT_NV)
+    {
+        results.emplace_back(THSVS_ACCESS_SHADING_RATE_READ_NV);
+    }
+    if (_flags & VK_IMAGE_USAGE_TRANSFER_SRC_BIT)
+    {
+        results.emplace_back(THSVS_ACCESS_TRANSFER_READ);
+    }
+    /*
+    if (_flags & VK_IMAGE_USAGE_TRANSFER_DST_BIT)
+    {
+        results.emplace_back(THSVS_ACCESS_TRANSFER_WRITE);
+    }
+    */
+    if (_flags & VK_IMAGE_USAGE_FRAGMENT_DENSITY_MAP_BIT_EXT)
+    {
+        results.emplace_back(THSVS_ACCESS_FRAGMENT_DENSITY_MAP_READ_EXT);
+    }
+
+    if (results.empty() || (_flags & VK_IMAGE_USAGE_STORAGE_BIT))
+    {
+        results.emplace_back(THSVS_ACCESS_ANY_SHADER_READ_OTHER);
+        results.emplace_back(THSVS_ACCESS_ANY_SHADER_WRITE);
+    }
+
+    return results;
 }
 
 static VkAccessFlags accessFlagsFromImageUsage(const VkImageUsageFlags usage_flags)
@@ -599,32 +700,31 @@ void ResourceContextImpl::setBufferInitialDataUploadBuffer(VulkanResource* resou
     const VkBufferCreateInfo* p_info = reinterpret_cast<VkBufferCreateInfo*>(resource->Info);
 	const uint32_t transfer_queue_idx = device->QueueFamilyIndices().Transfer;
 
-    const VkBufferMemoryBarrier memory_barrier0
-    {
-        VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER,
-        nullptr,
-        0u,
-        VK_ACCESS_TRANSFER_WRITE_BIT,
-        VK_QUEUE_FAMILY_IGNORED,
-        VK_QUEUE_FAMILY_IGNORED,
-        reinterpret_cast<VkBuffer>(resource->Handle),
-        0u,
-        p_info->size
+    const ThsvsAccessType transfer_access_types[1]{
+        THSVS_ACCESS_TRANSFER_WRITE
     };
-    const VkBufferMemoryBarrier memory_barrier1
-    {
-        VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER,
-        nullptr,
-        VK_ACCESS_TRANSFER_WRITE_BIT,
-        accessFlagsFromBufferUsage(p_info->usage),
-        initial_data->DestinationQueueFamily != transfer_queue_idx ? transfer_queue_idx : VK_QUEUE_FAMILY_IGNORED,
-		initial_data->DestinationQueueFamily != transfer_queue_idx ? initial_data->DestinationQueueFamily : VK_QUEUE_FAMILY_IGNORED,
+
+    std::vector<ThsvsAccessType> possible_access_types = thsvsAccessTypesFromBufferUsage((VkBufferUsageFlagBits)p_info->usage);
+
+    ThsvsBufferBarrier post_transfer_barrier{
+        1u,
+        transfer_access_types,
+        static_cast<uint32_t>(possible_access_types.size()),
+        possible_access_types.data(),
+        VK_QUEUE_FAMILY_IGNORED,
+        VK_QUEUE_FAMILY_IGNORED,
         (VkBuffer)resource->Handle,
         0u,
         p_info->size
     };
 
-    
+	if (p_info->sharingMode == VK_SHARING_MODE_EXCLUSIVE)
+	{
+		// update for proper ownership transfer
+		post_transfer_barrier.srcQueueFamilyIndex = initial_data->DestinationQueueFamily != transfer_queue_idx ? transfer_queue_idx : VK_QUEUE_FAMILY_IGNORED;
+		post_transfer_barrier.dstQueueFamilyIndex = initial_data->DestinationQueueFamily != transfer_queue_idx ? initial_data->DestinationQueueFamily : VK_QUEUE_FAMILY_IGNORED;
+	}
+
     auto& transfer_system = ResourceTransferSystem::GetTransferSystem();
     auto cmd = transfer_system.TransferCmdBuffer();
     auto guard = transfer_system.AcquireSpinLock();
@@ -639,9 +739,8 @@ void ResourceContextImpl::setBufferInitialDataUploadBuffer(VulkanResource* resou
         offset += initial_data[i].DataSize;
     }
 
-    vkCmdPipelineBarrier(cmd, VK_PIPELINE_STAGE_ALL_COMMANDS_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, 0u, 0u, 0u, 1u, &memory_barrier0, 0u, nullptr);
     vkCmdCopyBuffer(cmd, upload_buffer->Buffer, reinterpret_cast<VkBuffer>(resource->Handle), static_cast<uint32_t>(buffer_copies.size()), buffer_copies.data());
-    vkCmdPipelineBarrier(cmd, VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_ALL_COMMANDS_BIT, 0u, 0u, nullptr, 1u, &memory_barrier1, 0u, nullptr);
+    thsvsCmdPipelineBarrier(cmd, nullptr, 1u, &post_transfer_barrier, 0u, nullptr);
     
 }
 
@@ -649,36 +748,51 @@ void ResourceContextImpl::setImageInitialData(VulkanResource* resource, const si
 {
     const VkImageCreateInfo* info = reinterpret_cast<VkImageCreateInfo*>(resource->Info);
 
-    const VkImageMemoryBarrier barrier0
+    constexpr static ThsvsAccessType transfer_access_types[1]{
+        THSVS_ACCESS_TRANSFER_WRITE
+    };
+
+    // required, unlike with buffers. forces a layout transition.
+    const ThsvsImageBarrier pre_transfer_barrier
     {
-        VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
-        nullptr,
         0u,
-        VK_ACCESS_TRANSFER_WRITE_BIT,
-        VK_IMAGE_LAYOUT_UNDEFINED,
-        VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
-        VK_QUEUE_FAMILY_IGNORED, // acquisition by a queue the first time is free and doesn't need a transfer
+        nullptr,
+        1u,
+        transfer_access_types,
+        THSVS_IMAGE_LAYOUT_GENERAL,
+        THSVS_IMAGE_LAYOUT_OPTIMAL,
+        VK_FALSE,
         VK_QUEUE_FAMILY_IGNORED,
-        reinterpret_cast<VkImage>(resource->Handle),
+        VK_QUEUE_FAMILY_IGNORED,
+        (VkImage)resource->Handle,
         VkImageSubresourceRange{ VK_IMAGE_ASPECT_COLOR_BIT, 0u, info->mipLevels, 0u, info->arrayLayers }
+    };
+
+
+    auto post_transfer_access_types = thsvsAccessTypesFromImageUsage(info->usage);
+
+    ThsvsImageBarrier post_transfer_barrier
+    {
+        1u,
+        transfer_access_types,
+        static_cast<uint32_t>(post_transfer_access_types.size()),
+        post_transfer_access_types.data(),
+        THSVS_IMAGE_LAYOUT_OPTIMAL,
+        THSVS_IMAGE_LAYOUT_OPTIMAL,
+        VK_FALSE,
+        VK_QUEUE_FAMILY_IGNORED,
+        VK_QUEUE_FAMILY_IGNORED,
+        (VkImage)resource->Handle,
+        VkImageSubresourceRange { VK_IMAGE_ASPECT_COLOR_BIT, 0u, info->mipLevels, 0u, info->arrayLayers }
     };
 
 	const uint32_t transfer_queue_idx = device->QueueFamilyIndices().Transfer;
 
-    const VkImageMemoryBarrier barrier1
+    if (info->sharingMode == VK_SHARING_MODE_EXCLUSIVE)
     {
-            VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
-            nullptr,
-            VK_ACCESS_TRANSFER_WRITE_BIT,
-            accessFlagsFromImageUsage(info->usage),
-            VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
-            imageLayoutFromUsage(info->usage),
-			transfer_queue_idx != initial_data->DestinationQueueFamily ? transfer_queue_idx : VK_QUEUE_FAMILY_IGNORED,
-			transfer_queue_idx != initial_data->DestinationQueueFamily ? initial_data->DestinationQueueFamily : VK_QUEUE_FAMILY_IGNORED,
-            reinterpret_cast<VkImage>(resource->Handle),
-            VkImageSubresourceRange{ VK_IMAGE_ASPECT_COLOR_BIT, 0u, info->mipLevels, 0u, info->arrayLayers }
-    };
-
+        post_transfer_barrier.srcQueueFamilyIndex = transfer_queue_idx != initial_data->DestinationQueueFamily ? transfer_queue_idx : VK_QUEUE_FAMILY_IGNORED;
+        post_transfer_barrier.dstQueueFamilyIndex = transfer_queue_idx != initial_data->DestinationQueueFamily ? initial_data->DestinationQueueFamily : VK_QUEUE_FAMILY_IGNORED;
+    }
 
     auto& transfer_system = ResourceTransferSystem::GetTransferSystem();
     {
@@ -688,6 +802,7 @@ void ResourceContextImpl::setImageInitialData(VulkanResource* resource, const si
         VkCommandBuffer cmd = transfer_system.TransferCmdBuffer();
         std::vector<VkBufferImageCopy> buffer_image_copies;
         size_t copy_offset = 0u;
+
         for (uint32_t i = 0u; i < num_data; ++i)
         {
             buffer_image_copies.emplace_back(VkBufferImageCopy{
@@ -703,9 +818,10 @@ void ResourceContextImpl::setImageInitialData(VulkanResource* resource, const si
             upload_buffer->SetData(initial_data[i].Data, initial_data[i].DataSize, copy_offset);
             copy_offset += initial_data[i].DataSize;
         }
-        vkCmdPipelineBarrier(cmd, VK_PIPELINE_STAGE_ALL_COMMANDS_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, 0u, 0u, nullptr, 0u, nullptr, 1u, &barrier0);
+
+        thsvsCmdPipelineBarrier(cmd, nullptr, 0u, nullptr, 1u, &pre_transfer_barrier);
         vkCmdCopyBufferToImage(cmd, upload_buffer->Buffer, reinterpret_cast<VkImage>(resource->Handle), VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, static_cast<uint32_t>(buffer_image_copies.size()), buffer_image_copies.data());
-        vkCmdPipelineBarrier(cmd, VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_ALL_COMMANDS_BIT, 0u, 0u, nullptr, 0u, nullptr, 1u, &barrier1);
+        thsvsCmdPipelineBarrier(cmd, nullptr, 0u, nullptr, 1u, &post_transfer_barrier);
     }
 
 }
