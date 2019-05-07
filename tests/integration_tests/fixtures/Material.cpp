a40740bd898c13ae9f46a52a00fd813bbdf0abe6
Keegan <jonesk74@uw.edu>
2019-05-06 23:15:44 -0700

    Material updates
    - Fix incredibly inefficient and headass filesystem search
    - Re-arrange things so flow of code feels cleaner in the file
    - Turns out we gotta clamp all the data coming in
    - Async loading of textures is go

--- tests/integration_tests/fixtures/Material.cpp
+++ tests/integration_tests/fixtures/Material.cpp
@@ -1,17 +1,18 @@
 #include "Material.hpp"
-#include "stb/stb_image.h"
-#include "ResourceContext.hpp"
-#include "RenderingContext.hpp"
-#include "LogicalDevice.hpp"
 #include "DescriptorBinder.hpp"
+#include "LogicalDevice.hpp"
+#include "ResourceContext.hpp"
+#include "ResourceLoader.hpp"
+#include "RenderingContext.hpp"
 #include <array>
-#include <mutex>
 #include <condition_variable>
 #include <experimental/filesystem>
+#include <mutex>
+#include <unordered_set>
+#pragma warning(push, 1)
 #include "easylogging++.h"
 #include "mango/include/mango/mango.hpp"
-#include <unordered_set>
-#include "ResourceLoader.hpp"
+#pragma warning(pop)
 
 namespace stdfs = std::experimental::filesystem;
 
@@ -28,12 +29,70 @@ constexpr static auto METALLIC_TEXTURE_TYPE = TEXTURE_TYPE_VAL<texture_type::Met
 constexpr static auto EMISSIVE_TEXTURE_TYPE = TEXTURE_TYPE_VAL<texture_type::Emissive>;
 constexpr static auto NORMAL_TEXTURE_TYPE = TEXTURE_TYPE_VAL<texture_type::Normal>;
 
+static std::mutex debugCreationMutex;
+static std::condition_variable cVar;
+static VulkanResource* failedLoadDebugTexture{ nullptr };
+static VulkanResource* loadingMaterialTexture{ nullptr };
+static Material debugMaterial;
+
 struct loaded_texture_data_t
 {
     loaded_texture_data_t(const char* fname) : bitmap(fname) {}
     mango::Bitmap bitmap;
     VulkanResource* resource{ nullptr };
 };
+
+std::experimental::filesystem::path FindFile(const std::string& fname, const char* starting_dir, size_t depth_limit) {
+    static std::unordered_map<std::string, stdfs::path> foundPathsCache;
+    static std::mutex cacheMutex;
+
+    stdfs::path starting_path(stdfs::canonical(starting_dir));
+
+    stdfs::path file_name_path(fname);
+    file_name_path = file_name_path.filename();
+
+    if (stdfs::exists(file_name_path))
+    {
+        return file_name_path;
+    }
+
+    {
+        std::lock_guard cacheGuard(cacheMutex);
+        auto iter = foundPathsCache.find(fname);
+        if (iter != foundPathsCache.end())
+        {
+            return iter->second;
+        }
+    }
+
+    stdfs::path file_path = starting_path;
+
+    for (size_t i = 0; i < depth_limit; ++i)
+    {
+        file_path = file_path.parent_path();
+    }
+
+    for (auto& dir_entry : stdfs::recursive_directory_iterator(file_path))
+    {
+        if (dir_entry == starting_path)
+        {
+            continue;
+        }
+
+        stdfs::path entry_path(dir_entry);
+
+        if (!stdfs::is_regular_file(entry_path) && entry_path.has_filename() && (entry_path.filename() == file_name_path))
+        {
+            std::lock_guard cacheGuard(cacheMutex);
+            auto iter = foundPathsCache.emplace(fname, entry_path);
+            return iter.first->second;
+        }
+    }
+
+
+
+    return stdfs::path();
+}
 
 void* loadImageDataFn(const char* fname, void* user_data)
 {
@@ -43,7 +102,57 @@ void* loadImageDataFn(const char* fname, void* user_data)
     const auto* device = RenderingContext::Get().Device();
     const uint32_t graphics_queue_idx = device->QueueFamilyIndices().Graphics;
 
-    loaded_texture_data_t* result = new loaded_texture_data_t(fname);
+    stdfs::path file_path(fname);
+    std::string fileStr(fname);
+
+    if (!stdfs::exists(file_path))
+    {
+        file_path = FindFile(file_path.string(), "../../", 2u);
+    }
+
+    if (!stdfs::exists(file_path))
+    {
+        LOG(ERROR) << "Requested file path did not exist!";
+        LOG(ERROR) << "File Path: " << fname;
+
+        namespace fs = std::experimental::filesystem;
+        static fs::path DEBUG_TEXTURE_PATH{ "../../../../assets/debug/FailedTexLoad.png" };
+        if (!fs::exists(DEBUG_TEXTURE_PATH))
+        {
+            const std::string curr_dir = fs::current_path().string();
+            DEBUG_TEXTURE_PATH = FindFile(DEBUG_TEXTURE_PATH.filename().string(), curr_dir.c_str(), 5u);
+        }
+
+        fileStr = DEBUG_TEXTURE_PATH.string();
+
+    }
+
+    const static Format FORMAT_R16G16B16(16, mango::Format::UNORM, mango::Format::RGB, 16, 16, 16, 0);
+    static const std::map<mango::Format, VkComponentMapping> formatComponentMappings{
+        { FORMAT_A8, VkComponentMapping{ VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_R } },
+        { FORMAT_L8, VkComponentMapping{ VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_R } },
+        { FORMAT_R16, VkComponentMapping{ VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_R } },
+        { FORMAT_L16, VkComponentMapping{ VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_R } },
+        { FORMAT_L8A8, VkComponentMapping{ VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_G, VK_COMPONENT_SWIZZLE_ZERO, VK_COMPONENT_SWIZZLE_ZERO } },
+        { FORMAT_R8G8B8A8, VkComponentMapping{ VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_G, VK_COMPONENT_SWIZZLE_B, VK_COMPONENT_SWIZZLE_A } },
+        { FORMAT_R8G8B8X8, VkComponentMapping{ VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_G, VK_COMPONENT_SWIZZLE_B, VK_COMPONENT_SWIZZLE_ZERO }},
+        { FORMAT_B8G8R8, VkComponentMapping{ VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_G, VK_COMPONENT_SWIZZLE_B, VK_COMPONENT_SWIZZLE_ZERO }},
+        { FORMAT_B8G8R8X8, VkComponentMapping{ VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_G, VK_COMPONENT_SWIZZLE_B, VK_COMPONENT_SWIZZLE_ZERO }},
+        { FORMAT_B8G8R8A8, VkComponentMapping{ VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_G, VK_COMPONENT_SWIZZLE_B, VK_COMPONENT_SWIZZLE_A }},
+        { FORMAT_R16G16B16, VkComponentMapping{ VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_G, VK_COMPONENT_SWIZZLE_B, VK_COMPONENT_SWIZZLE_ZERO }}
+    };
+
+
+    loaded_texture_data_t* result = new loaded_texture_data_t(fileStr.c_str());
+
+    auto iter = formatComponentMappings.find(result->bitmap.format);
+    if (iter == std::cend(formatComponentMappings))
+    {
+        LOG(ERROR) << "Couldn't find valid component mapping for imported file format!";
+        throw std::runtime_error("Bad image format loaded.");
+    }
+
+    const VkComponentMapping view_mapping = iter->second;
 
     if (result->bitmap.format == FORMAT_R8G8B8 || result->bitmap.format == FORMAT_B8G8R8A8 || result->bitmap.format == FORMAT_B8G8R8 ||
         result->bitmap.format == FORMAT_R8G8B8X8 || result->bitmap.format == FORMAT_B8G8R8X8)
@@ -53,10 +162,18 @@ void* loadImageDataFn(const char* fname, void* user_data)
         result->bitmap = std::move(dest_blit);
     }
 
+    if (result->bitmap.format == FORMAT_R16G16B16)
+    {
+        Bitmap dest_blit(result->bitmap.width, result->bitmap.height, FORMAT_R16G16B16A16);
+        dest_blit.blit(0, 0, result->bitmap);
+        result->bitmap = std::move(dest_blit);
+    }
+
     static const std::map<mango::Format, VkFormat> formatConversionMap{
         { FORMAT_A8, VK_FORMAT_R8_UNORM },
         { FORMAT_L8, VK_FORMAT_R8_UNORM },
         { FORMAT_R16, VK_FORMAT_R16_UNORM },
+        { FORMAT_L16, VK_FORMAT_R16_UNORM },
         { FORMAT_L8A8, VK_FORMAT_R8G8_UNORM },
         { FORMAT_R8G8B8A8, VK_FORMAT_R8G8B8A8_UNORM }
     };
@@ -123,7 +240,7 @@ void* loadImageDataFn(const char* fname, void* user_data)
         VK_NULL_HANDLE,
         VK_IMAGE_VIEW_TYPE_2D,
         image_format,
-        VkComponentMapping{ VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_G, VK_COMPONENT_SWIZZLE_B, VK_COMPONENT_SWIZZLE_A },
+        view_mapping,
         VkImageSubresourceRange{ VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1 }
     };
 
@@ -146,14 +263,14 @@ void materialTextureLoadedCallback(void* instance, void* data_ptr, void* user_da
     reinterpret_cast<Material*>(instance)->textureLoadedCallback(data_ptr, user_data);
 }
 
-static std::mutex debugCreationMutex;
-static std::condition_variable cVar;
-static VulkanResource* failedLoadDebugTexture{ nullptr };
-static VulkanResource* loadingMaterialTexture{ nullptr };
-static Material debugMaterial;
-
 float extract_valid_material_param(const float input)
 {
+
+    if (std::isnan(input))
+    {
+        return 0.0f;
+    }
+
     // we can get weird values from the importer, so check for reasonable quantities here
     if (input < -1.0e8f || input > 1.0e8f)
     {
@@ -168,73 +285,15 @@ float extract_valid_material_param(const float input)
     return input;
 }
 
-std::experimental::filesystem::path FindFile(const std::string& fname, const char* starting_dir, size_t depth_limit) {
-    static std::unordered_map<std::string, stdfs::path> foundPathsCache;
-    static std::mutex cacheMutex;
-
-    stdfs::path starting_path(stdfs::canonical(starting_dir));
-
-    stdfs::path file_name_path(fname);
-    file_name_path = file_name_path.filename();
-
-    if (stdfs::exists(file_name_path))
-    {
-        return file_name_path;
-    }
-
-    {
-        std::lock_guard cacheGuard(cacheMutex);
-        auto iter = foundPathsCache.find(fname);
-        if (iter != foundPathsCache.end())
-        {
-            return iter->second;
-        }
-    }
-
-    stdfs::path file_path = starting_path;
-
-    for (size_t i = 0; i < depth_limit; ++i)
-    {
-        file_path = file_path.parent_path();
-    }
-
-    for (auto& dir_entry : stdfs::recursive_directory_iterator(file_path))
-    {
-        if (dir_entry == starting_path)
-        {
-            continue;
-        }
-
-        if (stdfs::is_directory(dir_entry))
-        {
-            continue;
-        }
-
-        if (!stdfs::is_regular_file(dir_entry))
-        {
-            continue;
-        }
-
-        stdfs::path entry_path(dir_entry);
-
-        if (entry_path.extension() != file_name_path.extension())
-        {
-            continue;
-        }
-
-        if (entry_path.has_filename() && (entry_path.filename() == file_name_path))
-        {
-            std::lock_guard cacheGuard(cacheMutex);
-            auto iter = foundPathsCache.emplace(fname, entry_path);
-            return iter.first->second;
-        }
-
-    }
-
-    
-    
-    return stdfs::path();
+glm::vec3 extract_valid_material_param(const glm::vec3& in) noexcept
+{
+    return glm::vec3{
+        extract_valid_material_param(in.x),
+        extract_valid_material_param(in.y),
+        extract_valid_material_param(in.z)
+    };
 }
+
 
 Material::Material(const tinyobj_opt::material_t& mtl, const char* mtl_dir)
 {
@@ -293,7 +352,8 @@ Material::Material(const tinyobj_opt::material_t& mtl, const char* mtl_dir)
 
 Material::Material(Material&& other) noexcept : opaque(other.opaque), textureToggles(std::move(other.textureToggles)), parameters(std::move(other.parameters)), paramsUbo(std::move(other.paramsUbo)),
     albedoMap(std::move(other.albedoMap)), normalMap(std::move(other.normalMap)), ambientOcclusionMap(std::move(other.ambientOcclusionMap)), metallicMap(std::move(other.metallicMap)),
-    roughnessMap(std::move(other.roughnessMap)) {
+    roughnessMap(std::move(other.roughnessMap))
+{
     other.paramsUbo = nullptr;
     other.albedoMap = nullptr;
     other.normalMap = nullptr;
@@ -398,12 +458,65 @@ Material::~Material()
 
 void Material::Bind(VkCommandBuffer cmd, const VkPipelineLayout layout, DescriptorBinder& binder)
 {
+
     vkCmdPushConstants(cmd, layout, VK_SHADER_STAGE_FRAGMENT_BIT, 0u, sizeof(texture_toggles_t), &textureToggles);
-    binder.BindResourceToIdx("Material", "AlbedoMap", VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, albedoMap);
-    binder.BindResourceToIdx("Material", "NormalMap", VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, normalMap);
-    binder.BindResourceToIdx("Material", "AmbientOcclusionMap", VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, ambientOcclusionMap);
-    binder.BindResourceToIdx("Material", "MetallicMap", VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, metallicMap);
-    binder.BindResourceToIdx("Material", "RoughnessMap", VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, roughnessMap);
+
+    if (!textureToggles.materialLoading)
+    {
+        binder.BindResourceToIdx("Material", "Parameters", VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, paramsUbo);
+    }
+
+    if (textureToggles.hasAlbedoMap)
+    {
+        binder.BindResourceToIdx("Material", "AlbedoMap", VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, albedoMap);
+    }
+    
+    if (textureToggles.hasAlphaMap)
+    {
+        binder.BindResourceToIdx("Material", "AlphaMap", VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, alphaMap);
+    }
+
+    if (textureToggles.hasSpecularMap)
+    {
+        binder.BindResourceToIdx("Material", "SpecularMap", VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, specularMap);
+    }
+
+    if (textureToggles.hasBumpMap)
+    {
+        binder.BindResourceToIdx("Material", "NormalMap", VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, bumpMap);
+    }
+
+    if (textureToggles.hasDisplacementMap)
+    {
+
+    }
+
+    if (textureToggles.hasNormalMap)
+    {
+        binder.BindResourceToIdx("Material", "NormalMap", VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, normalMap);
+    }
+
+    if (textureToggles.hasAmbientOcclusionMap)
+    {
+        binder.BindResourceToIdx("Material", "AmbientOcclusionMap", VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, ambientOcclusionMap);
+    }
+
+    if (textureToggles.hasMetallicMap)
+    {
+        binder.BindResourceToIdx("Material", "MetallicMap", VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, metallicMap);
+    }
+
+    if (textureToggles.hasRoughnessMap)
+    {
+        binder.BindResourceToIdx("Material", "RoughnessMap", VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, roughnessMap);
+    }
+
+    if (textureToggles.hasEmissiveMap)
+    {
+
+    }
+
+    assert(!(textureToggles.hasNormalMap && textureToggles.hasBumpMap));
     binder.Update();
     binder.Bind(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS);
 }
@@ -423,7 +536,16 @@ Material Material::CreateDebugMaterial()
     tinyobj_opt::material_t debug_material;
     debug_material.name = "DEBUG_MATERIAL";
     result.createUBO(debug_material);
+    result.loadTexture("", debug_material.name, "", AMBIENT_TEXTURE_TYPE);
     result.loadTexture("", debug_material.name, "", DIFFUSE_TEXTURE_TYPE);
+    result.loadTexture("", debug_material.name, "", SPECULAR_TEXTURE_TYPE);
+    result.loadTexture("", debug_material.name, "", BUMP_TEXTURE_TYPE);
+    result.loadTexture("", debug_material.name, "", DISPLACEMENT_TEXTURE_TYPE);
+    result.loadTexture("", debug_material.name, "", ALPHA_TEXTURE_TYPE);
+    result.loadTexture("", debug_material.name, "", ROUGHNESS_TEXTURE_TYPE);
+    result.loadTexture("", debug_material.name, "", METALLIC_TEXTURE_TYPE);
+    result.loadTexture("", debug_material.name, "", EMISSIVE_TEXTURE_TYPE);
+    result.loadTexture("", debug_material.name, "", NORMAL_TEXTURE_TYPE);
     return result;
 }
 
@@ -434,14 +556,14 @@ const Material::material_texture_counter_t& Material::TextureCounter() noexcept
 
 void Material::setParameters(const tinyobj_opt::material_t& mtl)
 {
-    parameters.ambient = glm::vec3(mtl.ambient[0], mtl.ambient[1], mtl.ambient[2]);
-    parameters.diffuse = glm::vec3(mtl.diffuse[0], mtl.diffuse[1], mtl.diffuse[2]);
-    parameters.specular = glm::vec3(mtl.specular[0], mtl.specular[1], mtl.specular[2]);
-    parameters.transmittance = glm::vec3(mtl.transmittance[0], mtl.transmittance[1], mtl.transmittance[2]);
-    parameters.emission = glm::vec3(mtl.emission[0], mtl.emission[1], mtl.emission[2]);
-    parameters.shininess = mtl.shininess;
-    parameters.ior = mtl.ior;
-    parameters.dissolve = mtl.dissolve;
+    parameters.ambient = extract_valid_material_param(glm::vec3(mtl.ambient[0], mtl.ambient[1], mtl.ambient[2]));
+    parameters.diffuse = extract_valid_material_param(glm::vec3(mtl.diffuse[0], mtl.diffuse[1], mtl.diffuse[2]));
+    parameters.specular = extract_valid_material_param(glm::vec3(mtl.specular[0], mtl.specular[1], mtl.specular[2]));
+    parameters.transmittance = extract_valid_material_param(glm::vec3(mtl.transmittance[0], mtl.transmittance[1], mtl.transmittance[2]));
+    parameters.emission = extract_valid_material_param(glm::vec3(mtl.emission[0], mtl.emission[1], mtl.emission[2]));
+    parameters.shininess = extract_valid_material_param(mtl.shininess);
+    parameters.ior = extract_valid_material_param(mtl.ior);
+    parameters.dissolve = extract_valid_material_param(mtl.dissolve);
     parameters.illum = static_cast<int32_t>(mtl.illum);
     parameters.roughness = extract_valid_material_param(mtl.roughness);
     parameters.metallic = extract_valid_material_param(mtl.metallic);
@@ -513,33 +635,6 @@ void Material::loadTexture(std::string file_path_str, const std::string& materia
     }
 
     auto& loader = ResourceLoader::GetResourceLoader();
-    stdfs::path file_path(file_path_str);
-
-    if (!stdfs::exists(file_path))
-    {
-        file_path = FindFile(file_path.string(), init_dir, 2u);
-    }
-
-    if (!stdfs::exists(file_path))
-    {
-        LOG(ERROR) << "Requested file path did not exist!";
-        LOG(ERROR) << "File Path: " << file_path_str;
-
-        namespace fs = std::experimental::filesystem;
-        static fs::path DEBUG_TEXTURE_PATH{ "../../../../assets/debug/FailedTexLoad.png" };
-        if (!fs::exists(DEBUG_TEXTURE_PATH))
-        {
-            const std::string curr_dir = fs::current_path().string();
-            DEBUG_TEXTURE_PATH = FindFile(DEBUG_TEXTURE_PATH.filename().string(), curr_dir.c_str(), 5u);
-        }
-
-        const std::string DEBUG_TEXTURE_PATH_STR = DEBUG_TEXTURE_PATH.string();
-        loader.Load("MANGO", DEBUG_TEXTURE_PATH_STR.c_str(), (void*)this, materialTextureLoadedCallback, (void*)type_ptr);
-        return;
-    }
-
-    file_path_str = stdfs::canonical(file_path).string();
-
     loader.Load("MANGO", file_path_str.c_str(), (void*)this, materialTextureLoadedCallback, (void*)type_ptr);
 
 }
