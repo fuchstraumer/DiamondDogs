3f8f7e2f4262c0ac3a56227b002577446510e821
Keegan <jonesk74@uw.edu>
2020-01-05 16:09:48 -0800

    remove bad VS files

--- core/include/threading/critical_section.cpp
+++
@@ -1,9 +0,0 @@
-#include "critical_section.hpp"
-
-critical_section::raii_scoped_lock::raii_scoped_lock(critical_section& _section) noexcept
-{
-}
-
-critical_section::raii_scoped_lock::~raii_scoped_lock()
-{
-}
