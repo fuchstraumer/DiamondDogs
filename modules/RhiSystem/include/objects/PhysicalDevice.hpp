3481e317ac2b7da7afcacd4c8be8d81691f1cc29
fuchstraumer <fuchstraumer@gmail.com>
2025-09-26 23:02:51 -0700

    Massively simplify PhysicalDevice
    I will probably just remove this fully when I'm not wildly tired

--- modules/RhiSystem/include/objects/PhysicalDevice.hpp
+++ modules/RhiSystem/include/objects/PhysicalDevice.hpp
@@ -2,8 +2,7 @@
 #ifndef RHI_SYSTEM_PHYSICAL_DEVICE_HPP
 #define RHI_SYSTEM_PHYSICAL_DEVICE_HPP
 #include "RhiTypes.hpp"
-#include <vulkan/vulkan_core.h>
-#include <vector>
+#include "RhiHandle.hpp"
 #include <cstdint>
 
 namespace rhi 
@@ -12,7 +11,7 @@ namespace rhi
     class PhysicalDevice 
     {
     public:
-        explicit PhysicalDevice(VkInstance instance, uint32_t api_version);
+        explicit PhysicalDevice(InstanceHandle instance, uint32_t api_version);
         ~PhysicalDevice() = default;
         
         // Move-only
@@ -20,40 +19,12 @@ namespace rhi
         PhysicalDevice& operator=(const PhysicalDevice&) = delete;
         PhysicalDevice(PhysicalDevice&&) noexcept = default;
         PhysicalDevice& operator=(PhysicalDevice&&) noexcept = default;
-        
-        // Core access
-        VkPhysicalDevice vkHandle() const noexcept;
-        
-        // Properties
-        const VkPhysicalDeviceProperties& GetProperties() const noexcept;
-        
-        const VkPhysicalDeviceFeatures& GetFeatures() const noexcept;
-        
-        const VkPhysicalDeviceMemoryProperties& GetMemoryProperties() const noexcept;
-        
-        // Queue families
-        const QueueFamilyIndices& GetQueueFamilyIndices() const noexcept;
-        
-        const std::vector<VkQueueFamilyProperties>& GetQueueFamilyProperties() const noexcept;
-        
-        // Format support
-        VkFormatProperties GetFormatProperties(VkFormat format) const;
-        bool SupportsFormat(VkFormat format, VkImageTiling tiling, VkFormatFeatureFlags features) const;
-        VkFormat FindSupportedFormat(const std::vector<VkFormat>& candidates, 
-                                    VkImageTiling tiling, 
-                                    VkFormatFeatureFlags features) const;
+
+        PhysicalDeviceHandle Handle() const noexcept;
 
     private:
-        VkPhysicalDevice selectBestDevice(VkInstance instance, uint32_t api_version);
-        void queryProperties();
-        void findQueueFamilies();
-        
-        VkPhysicalDevice handle;
-        VkPhysicalDeviceProperties properties;
-        VkPhysicalDeviceFeatures features;
-        VkPhysicalDeviceMemoryProperties memoryProperties;
-        QueueFamilyIndices queueFamilyIndices;
-        std::vector<VkQueueFamilyProperties> queueFamilyProperties;
+        PhysicalDeviceHandle selectBestDevice(InstanceHandle instance, uint32_t api_version);    
+        PhysicalDeviceHandle handle;
     };
 
 } // namespace rhi
