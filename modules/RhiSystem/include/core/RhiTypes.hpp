6c290c98e84ed5e9567bcc3eb781fd51a26bddf2
fuchstraumer <fuchstraumer@gmail.com>
2025-09-28 18:12:33 -0700

    Oops, wrong capitalization in RHI types

--- modules/RhiSystem/include/core/RhiTypes.hpp
+++ modules/RhiSystem/include/core/RhiTypes.hpp
@@ -15,13 +15,13 @@ namespace rhi
      */
     struct PushConstantRange
     {
-        ShaderStageFlags stageFlags = ShaderStageFlags::All;
-        uint32_t offset = 0;           // Byte offset within push constant block
-        uint32_t size = 0;             // Size in bytes (must be multiple of 4 for DX12)
-        
+        ShaderStageFlags stageFlags = ShaderStageFlags::None;
+        uint32_t Offset{ 0 };           // Byte offset within push constant block
+        uint32_t Size{ 0 };             // Size in bytes (must be multiple of 4 for DX12)
+
         constexpr PushConstantRange() noexcept = default;
         constexpr PushConstantRange(ShaderStageFlags stages, uint32_t offset, uint32_t size) noexcept
-            : stageFlags{ stages }, offset{ offset }, size{ size }
+            : stageFlags{ stages }, Offset{ offset }, Size{ size }
         {
         }
     };
@@ -38,52 +38,52 @@ namespace rhi
 
     struct ClearDepthStencilValue
     {
-        float depth = 1.0f;
-        uint32_t stencil = 0;
+        float Depth{ 1.0f };
+        uint32_t Stencil{ 0 };
     };
 
     struct ColorAttachment
     {
-        ImageViewHandle imageView{};
-        LoadOp loadOp = LoadOp::Clear;
-        StoreOp storeOp = StoreOp::Store;
-        ClearColorValue clearValue{};
+        ImageViewHandle ImageView{};
+        LoadOp LoadOp = LoadOp::Clear;
+        StoreOp StoreOp = StoreOp::Store;
+        ClearColorValue ClearValue{};
     };
 
     struct DepthStencilAttachment
     {
-        ImageViewHandle imageView{};
-        LoadOp depthLoadOp = LoadOp::Clear;
-        StoreOp depthStoreOp = StoreOp::DontCare;
-        LoadOp stencilLoadOp = LoadOp::DontCare;
-        StoreOp stencilStoreOp = StoreOp::DontCare;
-        ClearDepthStencilValue clearValue{};
+        ImageViewHandle ImageView{};
+        LoadOp DepthLoadOp = LoadOp::Clear;
+        StoreOp DepthStoreOp = StoreOp::DontCare;
+        LoadOp StencilLoadOp = LoadOp::DontCare;
+        StoreOp StencilStoreOp = StoreOp::DontCare;
+        ClearDepthStencilValue ClearValue{};
     };
 
     struct Viewport
     {
-        float x = 0.0f;
-        float y = 0.0f;
-        float width = 0.0f;
-        float height = 0.0f;
-        float minDepth = 0.0f;
-        float maxDepth = 1.0f;
+        float PosX{ 0.0f };
+        float PosY{ 0.0f };
+        float Width{ 0.0f };
+        float Height{ 0.0f };
+        float MinDepth{ 0.0f };
+        float MaxDepth{ 1.0f };
     };
 
     struct Rect2D
     {
-        int32_t x = 0;
-        int32_t y = 0;
-        uint32_t width = 0;
-        uint32_t height = 0;
+        int32_t PosX{ 0 };
+        int32_t PosY{ 0 };
+        uint32_t Width{ 0 };
+        uint32_t Height{ 0 };
     };
 
     struct RenderingInfo
     {
-        std::span<const ColorAttachment> colorAttachments;
-        std::optional<DepthStencilAttachment> depthStencilAttachment = std::nullopt;
-        Rect2D renderArea{};
-        uint32_t layerCount = 1;
+        std::span<const ColorAttachment> ColorAttachments;
+        std::optional<DepthStencilAttachment> DepthStencilAttachment = std::nullopt;
+        Rect2D RenderArea{};
+        uint32_t LayerCount{ 1 };
     };
 
     struct SpecializationConstant
